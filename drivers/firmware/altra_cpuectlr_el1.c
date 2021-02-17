e4d23353d152ae2ca0babb3589eb30ff6ad25bf2
Hoan Tran <hoan.tran@amperecomputing.com>
2021-02-16 22:37:06 -0800

    NOUPSTREAM: firmware: Support Ampere Altra CPUECTLR_EL1 config driver
    Based on the spec, CPUECTLR_EL1 cannot configure on the fly
    
    " Accessing the CPUECTLR_EL1
    The CPU Extended Control Register can be written only when the system is
    idle. Arm recommends that you write to this register after a powerup
    reset before the MMU is enabled. "
    
    To support changing CPUECTLR_EL1 without the system reboot,
    here is the sequence:
    - Create a non-secure "Shared Memory" to store the CPUECTLR_EL1
      value
    - From kernel, the user configures the desired value into the "Shared
      Memory" region from a new kernel driver
    - From kernel, the user takes each core offline then online until finish
      all the cores
             + When the core is online, at first, it jumps into ATF (EL3)
             + At ATF (EL3), core read the "Shared Memory" to get the new
    CPUECTLR_EL1 config value
             + Core configures the new value into CPUECTLR_EL1 register
             + Core jumps into kernel and enable MMU.
    - All the cores run with the new CPUECTLR_EL1 value.
    
    This patch adds the new kernel driver to configure the NS-DRAM
    shared memory.
    
    It's for the customer and internal testing purpose.
    
    Signed-off-by: Hoan Tran <hoan.tran@amperecomputing.com>
    Signed-off-by: Bobo <lmw.bobo@gmail.com>

---
+++ drivers/firmware/altra_cpuectlr_el1.c
@@ -0,0 +1,173 @@
+// SPDX-License-Identifier: GPL-2.0
+/* Ampere Altra SoC CPUECTLR_EL1 Config Driver
+ *
+ * Copyright (C) 2021 Ampere Computing LLC
+ * Author: Hoan Tran <hoan@os.amperecomputing.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
+
+#include <linux/bitmap.h>
+#include <linux/bitfield.h>
+#include <linux/device.h>
+#include <linux/io.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/platform_device.h>
+#include <linux/types.h>
+
+#define CPU_CONFIG_SHARED_BUFFER		0x88990000
+#define CPU_CONFIG_SHARED_BUFFER_SIZE		0x20
+#define CPU_CONFIG_SIGNATURE_OFF		0x0
+#define CPU_CONFIG_CPUECTLR_EL1_WEN_MASK_OFF	0x8
+#define CPU_CONFIG_CPUECTLR_EL1_VALUE_OFF	0x10
+#define CPU_CONFIG_SIGNATURE			0x43504D41
+
+struct kobject *cpuectlr_el1_kobj;
+static void __iomem *cpuectlr_el1_addr;
+
+static u32 altra_config_read32(void __iomem *base, u32 reg)
+{
+	return readl_relaxed(base + reg);
+}
+
+static void altra_config_write32(void __iomem *base, u32 reg, u32 val)
+{
+	writel_relaxed(val, base + reg);
+}
+
+static u64 altra_config_read64(void __iomem *base, u32 reg)
+{
+	return readq_relaxed(base + reg);
+}
+
+static void altra_config_write64(void __iomem *base, u32 reg, u64 val)
+{
+	writeq_relaxed(val, base + reg);
+}
+
+static ssize_t signature_show(struct device *dev,
+				 struct device_attribute *attr,
+				 char *buf)
+{
+	return sprintf(buf, "0x%x\n",
+		       altra_config_read32(cpuectlr_el1_addr, CPU_CONFIG_SIGNATURE_OFF));
+}
+
+static ssize_t cpuectlr_el1_wen_mask_show(struct device *dev,
+				 struct device_attribute *attr,
+				 char *buf)
+{
+	return sprintf(buf, "0x%llx\n",
+		       altra_config_read64(cpuectlr_el1_addr, CPU_CONFIG_CPUECTLR_EL1_WEN_MASK_OFF));
+}
+
+static ssize_t cpuectlr_el1_wen_mask_store(struct device *device,
+				struct device_attribute *attr,
+				const char *buf,
+				size_t count)
+{
+	ssize_t rc;
+	long long val;
+
+	rc = kstrtoll(buf, 0, &val);
+	if (rc)
+		return rc;
+	altra_config_write64(cpuectlr_el1_addr, CPU_CONFIG_CPUECTLR_EL1_WEN_MASK_OFF, val);
+
+	return count;
+}
+
+static ssize_t cpuectlr_el1_value_show(struct device *dev,
+				 struct device_attribute *attr,
+				 char *buf)
+{
+	return sprintf(buf, "0x%llx\n",
+		       altra_config_read64(cpuectlr_el1_addr, CPU_CONFIG_CPUECTLR_EL1_VALUE_OFF));
+}
+
+static ssize_t cpuectlr_el1_value_store(struct device *device,
+				struct device_attribute *attr,
+				const char *buf,
+				size_t count)
+{
+	ssize_t rc;
+	long long val;
+
+	rc = kstrtoll(buf, 0, &val);
+	if (rc)
+		return rc;
+	altra_config_write64(cpuectlr_el1_addr, CPU_CONFIG_CPUECTLR_EL1_VALUE_OFF, val);
+
+	return count;
+}
+
+static DEVICE_ATTR_RO(signature);
+static DEVICE_ATTR_RW(cpuectlr_el1_wen_mask);
+static DEVICE_ATTR_RW(cpuectlr_el1_value);
+
+static struct attribute *cpuectlr_el1_attrs[] = {
+	&dev_attr_signature.attr,
+	&dev_attr_cpuectlr_el1_wen_mask.attr,
+	&dev_attr_cpuectlr_el1_value.attr,
+	NULL,
+};
+
+static const struct attribute_group cpuectlr_el1_group = {
+	.attrs = cpuectlr_el1_attrs,
+};
+
+static int __init altra_cpuectlr_init(void)
+{
+	int error;
+
+	/* We register the efi directory at /sys/firmware/altra_cpuectlr */
+	cpuectlr_el1_kobj = kobject_create_and_add("altra_cpuectlr", firmware_kobj);
+	if (!cpuectlr_el1_kobj) {
+		pr_err("Altra cpuectlr_el1: Firmware registration failed.\n");
+		return -ENOMEM;
+	}
+
+	error = sysfs_create_group(cpuectlr_el1_kobj, &cpuectlr_el1_group);
+	if (error) {
+		pr_err("Altra cpuectlr_el1: Sysfs attribute export failed with error %d.\n",
+		       error);
+	}
+
+	cpuectlr_el1_addr = (void __force *)ioremap(CPU_CONFIG_SHARED_BUFFER,
+						    CPU_CONFIG_SHARED_BUFFER_SIZE);
+
+	if (!cpuectlr_el1_addr) {
+		pr_err("Altra cpuectlr_el1: Failed to ioremap cpu config region\n");
+		error = -ENOMEM;
+	}
+
+	altra_config_write32(cpuectlr_el1_addr, CPU_CONFIG_SIGNATURE_OFF, CPU_CONFIG_SIGNATURE);
+
+	return error;
+}
+
+static void __exit altra_cpuectlr_exit(void)
+{
+	sysfs_remove_group(cpuectlr_el1_kobj, &cpuectlr_el1_group);
+}
+
+module_init(altra_cpuectlr_init);
+module_exit(altra_cpuectlr_exit);
+
+MODULE_DESCRIPTION("Altra SoC CPUECTLR_EL1 config");
+MODULE_LICENSE("GPL v2");
