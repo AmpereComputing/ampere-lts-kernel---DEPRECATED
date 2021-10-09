c0319fa1c3b25ea96e75da02304df2ec17f5185b
Bobo <lmw.bobo@gmail.com>
2021-10-09 14:33:37 +0000

    ampere_functions: add ras extension error tests
    Signed-off-by: Bobo <lmw.bobo@gmail.com>

--- tests/ampere_functions/ras/ras.sh
+++ tests/ampere_functions/ras/ras.sh
@@ -9,6 +9,7 @@ LOGFILE="${OUTPUT}/ras.txt"
 create_out_dir "${OUTPUT}"
 
 function ras_check_errors {
+    dmesg -C
     modprobe einj || return 1
     # start record
     trace-cmd start -e mc_event -e non_standard_event -e arm_event || return 1
@@ -16,9 +17,37 @@ function ras_check_errors {
     echo 0x1 > /sys/kernel/debug/apei/einj/error_type || return 1
     echo 0x1 > /sys/kernel/debug/apei/einj/error_inject || return 1
     trace-cmd show | grep "ARM Processor Err"
-    [ $? -ne 0 ] && return 1
+    if [ $? -ne 0 ]; then
+        # Hardware EINJ enabled in BIOS
+        dmesg | egrep "CPU[0-9]+: ERR"
+        [ $? -ne 0 ] && return 1
+    fi
+    trace-cmd stop
+    return 0
+}
+
+function ras_check_ext_errors {
+    dmesg -C
+    modprobe einj || return 1
+    # start record
+    trace-cmd start -e mc_event -e non_standard_event -e arm_event -e arm_ras_ext_event || return 1
+    # inject cpu errors
+    echo 0x1 > /sys/kernel/debug/apei/einj/error_type || return 1
+    echo 0x0 > /sys/kernel/debug/apei/einj/param3 || return 1
+    echo 0x1 > /sys/kernel/debug/apei/einj/flags || return 1
+    echo 0x1 > /sys/kernel/debug/apei/einj/error_inject || return 1
+    trace-cmd show | grep "ARM Processor Err"
+    if [ $? -ne 0 ]; then
+        # Hardware EINJ enabled in BIOS
+        dmesg | egrep "CPU[0-9]+: ERR"
+        [ $? -ne 0 ] && return 1
+    fi
+    trace-cmd stop
     return 0
 }
 
 ras_check_errors
 check_return altra:ras
+
+ras_check_ext_errors
+check_return altra:ras_ext
